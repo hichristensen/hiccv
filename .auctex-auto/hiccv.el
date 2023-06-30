c51c728cf2ec2454b5a9b72b321eaafa68c7d6f6
Henrik I Christensen <hichristensen@gmail.com>
2023-06-30 10:11:28 -0400

    Mid-year update

---
+++ .auctex-auto/hiccv.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "hiccv"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "latin1") ("fontenc" "T1") ("bibtopic" "unitcntnoreset") ("hyperref" "colorlinks=true" "plainpages=false")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "fancyhdr"
+    "charter"
+    "fontenc"
+    "hiccurrvita"
+    "bibtopic"
+    "tocloft"
+    "graphicx"
+    "hyperref"
+    "microtype")
+   (LaTeX-add-environments
+    "sublist"
+    "subbulletlist"))
+ :latex)
+
---
+++ .auctex-auto/reports.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "reports"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "roadmap2023"
+    "christensen20:roadmap"
+    "christensen16:nextgenerrobot"
+    "hager15:towarscienautonphysicsystem"
+    "christensen13:frominterrobotusnationrobotroadm"
+    "christensen09:frominterrobotusnationrobotroadm"))
+ '(or :bibtex :latex))
+
